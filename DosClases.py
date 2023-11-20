73981b012962d6074deaca1b7fb0e004a5ac94fc
garhoy <andergarciahoyberg@gmail.com>
2023-11-20 20:18:58 +0100

    Reading and cleaning data

--- DosClases.py
+++ DosClases.py
@@ -1,23 +1,37 @@
 import pandas as pd
-
-
-
-
+from tensorflow.keras.preprocessing.image import load_img
+import random
+import PIL
 
 ############################################ READING AND EXTRACTING CHARACTERISTICS ################################################
 
 def Reading_csv():
-    csv = pd.read_csv("full_df.csv")
-    print(csv.head())
-    
+    df = pd.read_csv("full_df.csv")
+    df_filtering = df[(df['N'] == 1) | (df['D'] == 1)]
+
+    filename = df_filtering[["filename"]]
+    labels = df_filtering[["labels"]]
+
+    return labels,filename
 
 
+def Visualize_RandomImage(images):
+    random_file_name = random.choice(list(images.keys()))
+    random_image_data = images[random_file_name]
+    random_image_data.show()
 
 
+def Reading_images(filenames):
+    images = {}
 
+    for file_name in filenames['filename']:
+        image_data = load_img("Train/" + file_name)
+        images[file_name] = image_data
 
-
+    #Visualize_RandomImage(images)
+    return images
 
 
 if __name__ == "__main__":
-    Reading_csv()
+    labels, filename = Reading_csv()
+    images = Reading_images(filename)
