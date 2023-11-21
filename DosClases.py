483e1ba24ab2253897e07493ed3b3b1c251746ce
garhoy <andergarciahoyberg@gmail.com>
2023-11-21 20:38:38 +0100

    Creating generator

--- DosClases.py
+++ DosClases.py
@@ -1,37 +1,49 @@
 import pandas as pd
-from tensorflow.keras.preprocessing.image import load_img
-import random
-import PIL
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-############################################ READING AND EXTRACTING CHARACTERISTICS ################################################
+############################################ READING AND CLEANING DATAFRAME ################################################
 
 def Reading_csv():
     df = pd.read_csv("full_df.csv")
-    df_filtering = df[(df['N'] == 1) | (df['D'] == 1)]
+    df_filtering = df[df['labels'].isin(["['N']", "['D']"])]
+    df_filtering['labels'] = df_filtering['labels'].replace({"['N']": 0, "['D']": 1})
+    
+    df_filtering = df_filtering[['filename', 'labels']]
+    df_filtering = df_filtering.reset_index(drop=True)
+    df_filtering['labels'] = df_filtering['labels'].astype(str)
+    return df_filtering
 
-    filename = df_filtering[["filename"]]
-    labels = df_filtering[["labels"]]
+########################################## PREPROCESSING IMAGES #########################################################
 
-    return labels,filename
+def preprocces_images(df_filtering):
+    # Target size needed for VGG16
+    target_size = (224, 224)
 
+    datagen = ImageDataGenerator(
+        rescale=1./255,  
+        rotation_range=20,  
+        width_shift_range=0.2, 
+        height_shift_range=0.2,  
+        shear_range=0.2,  
+        zoom_range=0.2, 
+        horizontal_flip=True, 
+        fill_mode='nearest' 
+    )
 
-def Visualize_RandomImage(images):
-    random_file_name = random.choice(list(images.keys()))
-    random_image_data = images[random_file_name]
-    random_image_data.show()
+    df_filtering['filename'] = 'Train/' + df_filtering['filename']
 
+    train_generator = datagen.flow_from_dataframe(
+        dataframe=df_filtering,
+        directory='.',  
+        x_col='filename',  
+        y_col='labels',  
+        target_size=target_size,  
+        batch_size=32,
+        class_mode='binary' 
+    )
 
-def Reading_images(filenames):
-    images = {}
-
-    for file_name in filenames['filename']:
-        image_data = load_img("Train/" + file_name)
-        images[file_name] = image_data
-
-    #Visualize_RandomImage(images)
-    return images
-
+    return train_generator
 
 if __name__ == "__main__":
-    labels, filename = Reading_csv()
-    images = Reading_images(filename)
+    df_filtering = Reading_csv()
+    preprocces_images(df_filtering)
